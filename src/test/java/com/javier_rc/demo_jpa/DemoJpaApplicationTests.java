82bfd490020a9a287180f9a36a49ddd88d8577a0
Javier.rc <javier.rodriguez.external@eviden.com>
2025-04-10 10:23:38 +0200

    init

---
+++ src/test/java/com/javier_rc/demo_jpa/DemoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javier_rc.demo_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
